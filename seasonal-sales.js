e88cf134725909a779bfa3fe8f5a1d65a98b7fa7
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-07-10 11:13:21 -0500

    got products with departements to render

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,16 +1,65 @@
 let output = document.getElementById('output');
 
-let productsRequest = new XMLHttpRequest();
-let categoriesRequest = new XMLHttpRequest();
+let productsArr = [];
+let categoriesArr = [];
+let cardDataArr = [];
 
-function requestLoadHandler() {
-	let data = JSON.parse(event.target.responseText);
+function productsRequestLoadHandler() {
+	productsArr = JSON.parse(event.target.responseText).products;
+	console.log("products array", productsArr);
+	requestCategories();
 }
 
-productsRequest.addEventListener('load', requestLoadHandler);
-categoriesRequest.addEventListener('load', requestLoadHandler);
+function categoriesRequestLoadHander() {
+	categoriesArr = JSON.parse(event.target.responseText).categories;
+	console.log("categories array", categoriesArr);
+	buildCardDataArray();
+}
 
-productsRequest.open('GET', 'products.json');
-categoriesRequest.open('GET', 'categories.json');
-productsRequest.send();
-categoriesRequest.send();
+function requestProducts() {
+	let productsRequest = new XMLHttpRequest();
+	productsRequest.addEventListener('load', productsRequestLoadHandler);
+	productsRequest.open('GET', 'products.json');
+	productsRequest.send();
+}
+
+function requestCategories() {
+	let categoriesRequest = new XMLHttpRequest();
+	categoriesRequest.addEventListener('load', categoriesRequestLoadHander);
+	categoriesRequest.open('GET', 'categories.json');
+	categoriesRequest.send();
+}
+
+function buildCardDataArray() {
+	productsArr.forEach(function(product){
+		categoriesArr.forEach(function(category){
+			if (product.category_id === category.id) {
+				cardDataArr.push({
+					id: product.id,
+					name: product.name,
+					price: product.price,
+					category: category.name
+				});
+			}
+		});
+	});
+	console.log("card data array", cardDataArr)
+	buildCardDom();
+}
+
+function buildCardDom() {
+	cardDataArr.forEach(function(cardData){
+		output.innerHTML +=
+		`
+		<div class="card">
+			<h2>${cardData.name}</h2>
+			<h3>Department: ${cardData.category}</h3>
+			<p>Price: $${cardData.price}</p>
+		</div>
+		`;
+	});
+}
+
+
+// request products
+requestProducts();
